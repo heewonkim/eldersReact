fababe440c75dca9a6f9343001efe746823dcf9e
PaulaSante09 <paulamariesante@gmail.com>
2016-07-25 10:29:52 -0700

    add pop up button Daily exercise and Journal Prompt

---
+++ js/dailyexercise.js
@@ -0,0 +1,20 @@
+var r_text = new Array ();
+r_text[0] = "Text your child I love you";
+r_text[1] = "Email a family member about your day ";
+r_text[2] = "Create a Facebook status about your day";
+r_text[3] = "Skype a family member";
+r_text[4] = "Google something you want to learn about";
+r_text[5] = "Explore Google Hangouts"
+r_text[6] = "Post a selfie on Facebook";
+r_text[7] = "Add friends on Facebook";
+r_text[8] = "Help your friends to make their Facebook account ";
+r_text[9] = "Practice typing on the keyboard";
+r_text[10] = "Reach Level 5 in the typing game";
+r_text[11] = "Watch all the video tutorials";
+r_text[12] = "Try video chatting on Google Hangouts";
+r_text[13] = "Watch a music video in youtube";
+r_text[14] = "Add contacts on Skype";
+var i = Math.floor(15*Math.random())
+
+window.alert(r_text[i]);
+console.log("hi");
---
+++ js/journalprompt.js
@@ -0,0 +1,35 @@
+var r_text = new Array ();
+r_text[0] = "Write about your first job.";
+r_text[1] = "Write about your first week in college.";
+r_text[2] = "What are your best character traits?";
+r_text[3] = "What's your wildest dream?";
+r_text[4] = "What was your most precious childhood poessession?";
+r_text[5] = "What objects tell the story of your life?";
+r_text[6] = "What's the most memorable thing you ever got in the mail?";
+r_text[7] = "What Nicknames have you ever gotton or given?";
+r_text[8] = "What have you learned in your life?";
+r_text[9] = "What personal achievement make you proud?";
+r_text[10] = "What are some recent moments of happiness in your life?";
+r_text[11] = "Who is your family?";
+r_text[12] = "How do you define family?";
+r_text[13] = "What is your favorite place?";
+r_text[14] = "Do you know your way around city or town?";
+r_text[15] = "What's special about your hometown?";
+r_text[16] = "What motivates you?";
+r_text[17] = "What makes you happy?";
+r_text[18] = "What are you good at?";
+r_text[19] = "How do you find peace in your life?";
+r_text[20] = "What kind of feedback helps you improve?";
+r_text[21] = "How do you relieve stress?";
+r_text[22] = "What were the best movies you saw in the past year? ";
+r_text[23] = "What old television shows wpould you bring back?";
+r_text[24] = "What are the most memorable works of visual aret you have seen?";
+r_text[25] = "What are the best things you've read watched, heard or played?";
+r_text[26] = "What are your favorite games?";
+r_text[27] = "What news stories are you following?";
+r_text[28] = "What do you like about computers?";
+r_text[29] = "What are the modt meaningful relationships in your life?";
+var i = Math.floor(30*Math.random())
+
+window.alert(r_text[i]);
+console.log("hi")
